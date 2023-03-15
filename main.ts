72ba892c3c3ebac61aed18a962da1e159fa0bc80
monettys <124165786+monettys@users.noreply.github.com>
2023-03-15 14:32:20 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.clearScreen()
+basic.forever(function () {
+	
+})
